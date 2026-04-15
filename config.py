4b41779b53ef380a76ea1a4a537af5dbc2b8a6a2
Vaibhav M <vaibhavm99@outlook.com>
2026-04-15 10:12:34 +0530

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
@@ -8,5 +9,5 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 USER_AVATAR = os.path.join(BASE_DIR, "assets", "user.png")
 BOT_AVATAR = os.path.join(BASE_DIR, "assets", "bot.png")
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
 MODEL_NAME = "gemini-2.5-flash"
